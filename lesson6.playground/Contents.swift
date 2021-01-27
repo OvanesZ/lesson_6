3bc22ae4258feb2acc9aa86be4ad6324d9bbc183
Ovanes <o-zakharyan@mail.ru>
2021-01-27 20:40:51 +0300

    final

--- lesson6.playground/Contents.swift
+++ lesson6.playground/Contents.swift
@@ -6,6 +6,7 @@ import UIKit
 struct Queue<T> {
     private var array: [T] = []
     
+    
     mutating func addToQueue(_ element: T) {
         self.array.append(element)
     }
@@ -21,17 +22,28 @@ struct Queue<T> {
             }
         }
     }
+    subscript(safe index: Int) -> T? {
+            return array.indices ~= index ? array[index] : nil
+        }
 }
 
 
 
+
 var queue = Queue<Int>()
+
+
+
 
 queue.addToQueue(1)
 queue.addToQueue(2)
 queue.addToQueue(3)
 queue.addToQueue(4)
 queue.addToQueue(5)
+
+queue[safe: 0]
+queue[safe: 8]
+
 
 //var queue = Queue<String>()
 //
@@ -46,6 +58,7 @@ print(queue)
 queue.quitFromQueue()
 
 print(queue)
+queue[safe: 0]
 
 queue.removeAll {$0}
 
